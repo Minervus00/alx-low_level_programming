f44f5069ded60ed6c6e6155b53ada016f97a4865
Minervus00 <fayouedraogo@protonmail.com>
2022-06-06 16:31:39 -0700

    jjbhgtv88

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - buhbhbh
+ *
+ * Return: (0) ggh
+ */
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25; i >= 0; i--)
+		putchar(alpha[i]);
+	putchar('\n');
+
+	return (0);
+}
