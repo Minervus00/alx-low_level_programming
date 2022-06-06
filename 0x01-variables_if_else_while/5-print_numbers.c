26c7bd977a63e689837d9e8cf1cd1a2cc7738350
Minervus00 <fayouedraogo@protonmail.com>
2022-06-06 16:08:52 -0700

    jnhbn55

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - desdqcfc
+ *
+ * Return: (0) suuuu
+ */
+int main(void)
+{
+	char i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
