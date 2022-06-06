f871da6f4352222211918c5bd98174c805e3c681
Minervus00 <fayouedraogo@protonmail.com>
2022-06-06 16:35:43 -0700

    vtqzaa33

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - buhbhbh
+ *
+ * Return: (0) ggh
+ */
+int main(void)
+{
+	char alpha[] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+		putchar(alpha[i]);
+	putchar('\n');
+
+	return (0);
+}
