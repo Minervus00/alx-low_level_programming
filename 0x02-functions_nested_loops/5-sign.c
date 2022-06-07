6c25c9a105d1367c9fb2ead4f0cb3aecab11f8f0
Minervus00 <fayouedraogo@protonmail.com>
2022-06-07 16:16:36 -0700

    hjbjhbggg

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - the taskerr
+ * @n: the parameter checked
+ *
+ * Return: 1 if positive, 0 if is zero, -1 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (0);
+	}
+}
