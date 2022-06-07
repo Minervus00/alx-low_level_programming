06b8191e03e0e09505cfaa6f36bdf015e04df4c6
Minervus00 <fayouedraogo@protonmail.com>
2022-06-07 16:41:43 -0700

    hjbwxxx9

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - the taskerr
+ * @n: the parameterto give the abs
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
