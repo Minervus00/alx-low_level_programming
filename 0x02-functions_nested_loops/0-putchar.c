ee9172bf006dc2adebf146154f516599f58b7c39
Minervus00 <fayouedraogo@protonmail.com>
2022-06-07 12:12:27 -0700

    ghfvrd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - printer
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char var[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+		_putchar(var[i]);
+	_putchar('\n');
+	
+	return (0);
+}
