a3615e7c2ba3934b96208109435100b2156f07eb
Minervus00 <fayouedraogo@protonmail.com>
2022-06-08 05:15:07 -0700

    hklvw5

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 #include <limits.h>
+#include <stdio.h>
 
 /**
  * print_last_digit - the taskerr
- * @n: the parameterto give the abs
+ * @n: the parameter to give the abs
  *
  * Return: the last digit
  */
 int print_last_digit(int n)
 {
-	int x = abs(n) % 10;
+	int x = ((unsigned int) n) % 10;
 
 	_putchar('0' + x);
 	return (x);
