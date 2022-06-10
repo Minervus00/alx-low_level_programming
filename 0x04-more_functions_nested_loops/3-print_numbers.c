c451c61b1fc03fa2538ddc295cafe8c95bfd23ff
Minervus00 <fayouedraogo@protonmail.com>
2022-06-10 16:26:33 -0700

    ub66

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - on one line ints from 0 to 9 
+ * print_numbers - on one line ints from 0 to 9
  *
  * Return: 0 success
  */
@@ -9,8 +9,8 @@ void print_numbers(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 		_putchar('0' + i);
-	
+
 	_putchar('\n');
 }
