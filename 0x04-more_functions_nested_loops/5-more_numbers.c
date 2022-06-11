23340fbe53a81fb734762443c9daaa7dbf8c141b
Minervus00 <fayouedraogo@protonmail.com>
2022-06-10 17:00:59 -0700

    uyb64sss

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * most_numbers - digits from 0 to 14 10x
+ * more_numbers - digits from 0 to 14 10x
  *
  * Return: nooop
  */
-void most_numbers(void)
+void more_numbers(void)
 {
 	int i, j;
 
 	for (i = 1; i < 11; i++)
 	{
 		for (j = 0; j < 15; j++)
-			_putchar('0' + j);
+		{
+			if (j >= 10)
+				_putchar('1');
+
+			_putchar('0' + (j % 10));
+		}
 		_putchar('\n');
 	}
 }
