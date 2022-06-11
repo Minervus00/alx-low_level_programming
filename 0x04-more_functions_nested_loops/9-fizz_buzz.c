1a1ab42870e0b718080f1e30f76cc7005f6262c2
Minervus00 <fayouedraogo@protonmail.com>
2022-06-10 18:09:46 -0700

    yb66qqq8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - fizz_buzz
+ *
+ * Return: noooooop
+ */
+int main(void)
+{
+	int i;
+	char f[] = "Fizz", b[] = "Buzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("%s", f);
+
+		if (i % 5 == 0)
+			printf("%s", b);
+
+		else if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+		
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
