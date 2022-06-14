e004e6a5e96f5ccb47cb8ae2cc34d108e0b04730
Minervus00 <fayouedraogo@protonmail.com>
2022-06-14 07:44:47 -0700

    ggsss6331ss

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - kkkkkkkk
+ * @dest: llll
+ * @src: ooooo
+ *
+ * Return: nooooop
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (src);
+}
