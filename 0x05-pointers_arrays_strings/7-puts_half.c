ac3bdb9ed432096a8da8f78b3b483c75feee6a28
Minervus00 <fayouedraogo@protonmail.com>
2022-06-14 07:30:25 -0700

    ktfcqqq66

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - dfjybfddbd
+ * @str: thaaaat
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i, n, length = strlen(str);
+
+	printf("str = %s\n", str);
+	if (length > 0)
+	{
+		if (length % 2 != 0)
+			n = ((length - 1) / 2) + 1;
+		else
+			n = length / 2;
+
+		for (i = n; i <= length - 1; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
