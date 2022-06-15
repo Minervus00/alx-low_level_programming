9a1721956359997378e36e7cf66d820792731a71
Minervus00 <fayouedraogo@protonmail.com>
2022-06-14 18:21:49 -0700

    jhqaa6a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * _atoi - thattt
@@ -9,8 +11,17 @@
  */
 int _atoi(char *s)
 {
-	int i;
+	int i, g = 1, length = strlen(s) - 1;
+	char test[150] = "";
 
-	i = atoi(s);
+	for (i = 0; i <=  length; i++)
+	{
+		if (isdigit(s[i]))
+			strncat(test, &s[i], 1);
+
+		else if (strchr("-", s[i]) != NULL)
+			g *= -1;
+	}
+	i = g * atoi(test);
 	return (i);
 }
