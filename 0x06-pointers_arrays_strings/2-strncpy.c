753b772e8b2e1de71e94542814087fd8b0d5fbb1
Minervus00 <fayouedraogo@protonmail.com>
2022-06-16 09:25:44 -0700

    hgssq66

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - thattt
+ * @dest: destination
+ * @src: the src
+ * @n: bytes
+ *
+ * Return: char *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
