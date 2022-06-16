7ed090e3d6ce1be04df9b4df0bd0d6cc9da47013
Minervus00 <fayouedraogo@protonmail.com>
2022-06-16 09:20:12 -0700

    hhhhh99

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - thattt
+ * @dest: destination
+ * @src: the src
+ * @n: bytes
+ *
+ * Return: char *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strcat(dest, src, n));
+}
