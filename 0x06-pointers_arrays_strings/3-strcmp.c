213438a5e2f83f7746f50f3c274bed7614c729fb
Minervus00 <fayouedraogo@protonmail.com>
2022-06-16 09:29:51 -0700

    yqqqq

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - thattt
+ * @s1: str1
+ * @s2: str2
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
