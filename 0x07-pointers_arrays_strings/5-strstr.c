bff77a67d4fdec9b55c25bd9f39c0b462ee96d21
Minervus00 <fayouedraogo@protonmail.com>
2022-06-21 10:30:48 -0700

    hssqq5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strstr - fuct that
+ * @haystack: guserr
+ * @needle: lqsss
+ *
+ * Return: char *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
