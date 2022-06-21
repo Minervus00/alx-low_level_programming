d55b36702009fdb01c5e2e28ed5ae956848a1f28
Minervus00 <fayouedraogo@protonmail.com>
2022-06-21 10:20:52 -0700

    tq22qs

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strspn - hhdzqqal
+ * @s: khv55
+ * @accept: 65slkkss
+ *
+ * Return: nooooop
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
