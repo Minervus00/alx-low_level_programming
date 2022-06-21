e416ec49ae9cc72a861885f4827047cb8fe62ac3
Minervus00 <fayouedraogo@protonmail.com>
2022-06-21 10:26:41 -0700

    uuuujn

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strpbrk - hhdzqqal
+ * @s: khv55
+ * @accept: 65slkkss
+ *
+ * Return: nooooop
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
