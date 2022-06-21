82a21f63910c0198bef0b14f6cdb8f538b5dd3df
Minervus00 <fayouedraogo@protonmail.com>
2022-06-21 10:11:46 -0700

    yyfgvwqqq8

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _memcpy - function fills
+ * @dest: lll
+ * @src: kkkk
+ * @n: ddd
+ *
+ * Return: nothing
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+}
