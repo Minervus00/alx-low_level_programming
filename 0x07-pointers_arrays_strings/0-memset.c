ef4a985ad0f3d2d2abb5f537acb4efbc29b0807e
Minervus00 <fayouedraogo@protonmail.com>
2022-06-21 10:06:53 -0700

    yqzz55

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _memset - function fills
+ * @s: lll
+ * @b: kkkk
+ * @n: ddd
+ *
+ * Return: nothing
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
