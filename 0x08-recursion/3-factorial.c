aeec0c3fc94a514f45bb3eebc0b9cd3be36a4833
Minervus00 <fayouedraogo@protonmail.com>
2022-06-23 09:55:50 -0700

    rrffss22

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - hhhh
+ * @n: lll
+ *
+ * Return: noooooop
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
