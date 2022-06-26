c4ab99da74f95a6282f92c2fb198b793bb96f3d9
Minervus00 <fayouedraogo@protonmail.com>
2022-06-26 16:40:08 -0700

    hhgq33

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - ooo
+ * @argc: oiid
+ * @argv: jsdd
+ *
+ * Return: intt
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
