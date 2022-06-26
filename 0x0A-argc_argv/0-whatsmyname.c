69e7c21fd55ba16e3c738b829cb27b65ba1b25d4
Minervus00 <fayouedraogo@protonmail.com>
2022-06-26 16:26:49 -0700

    yyyq55

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - arggg
+ * @argc: hhh
+ * @argv: hdhbfd
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
