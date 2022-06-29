770d983e45c73e2b34c6e438a1743b843dbb95f2
Minervus00 <fayouedraogo@protonmail.com>
2022-06-29 06:13:48 -0700

    tgqss5

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,16 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *arr, *ads;
+	char *arr;
 
 	if (!(size))
 		return (NULL);
+
 	arr = malloc(size);
+	if (arr == NULL)
+		return (NULL);
+	
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	ads = arr;
-	free(arr);
-	return (ads);
+	return (arr);
 }
