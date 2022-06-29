7363da08611ef11cdf1a6f2df4f34c295876bc25
Minervus00 <fayouedraogo@protonmail.com>
2022-06-29 12:35:26 -0700

    ugqww

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	res = malloc(height);
+	res = (int **) malloc(height * sizeof(int *));
 	if (res == NULL)
 		return (NULL);
 
