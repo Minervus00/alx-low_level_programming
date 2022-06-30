913793ba9430936cb8aac9975d295415b7a7fa7f
Minervus00 <fayouedraogo@protonmail.com>
2022-06-30 10:46:33 -0700

    ggwwqq

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < l1; i++)
 		str[i] = s1[i];
-	for (i = 0; i < l2; i++)
+	for (i = 0; i < n; i++)
 		str[l1 + i] = s2[i];
+	str[l1 + n - 1] = '\0';
 	return (str);
 }
