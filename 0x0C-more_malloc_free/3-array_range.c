cbb5ace3e39c05fc27ff28a7d1c23ecedd76f213
Minervus00 <fayouedraogo@protonmail.com>
2022-06-30 13:48:33 -0700

    ttqwww

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * array_range - dd
+ * @min: ss
+ * @max: ww
+ *
+ * Return: int *
+ */
+int *array_range(int min, int max)
+{
+	int i, *arr, n;
+
+	if (min > max)
+		return (NULL);
+	n = max - min + 1;
+	arr = malloc(n);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < n; i++)
+		arr[i] = min + i;
+	return (arr);
+}
