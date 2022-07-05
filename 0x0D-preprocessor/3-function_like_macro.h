ef642ab9ef082c30bcaded6c4736683350b70891
Minervus00 <fayouedraogo@protonmail.com>
2022-07-05 07:12:45 -0700

    ffw22xx

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef __OBJ__MACRO__H__
 #define __OBJ__MACRO__H__
 
-#define #define ABS(x) (((x) > 0) ? (x) : -(x))
+#define ABS(x) (((x) > 0) ? (x) : -(x))
 
 #endif /*#indef __OBJ__MACRO__H__*/
