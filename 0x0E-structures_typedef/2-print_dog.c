17c22d0451a04248f9c35c1c16ae27f801780ea3
Minervus00 <fayouedraogo@protonmail.com>
2022-07-06 08:31:31 -0700

    xxxxwxcdx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,10 @@ void print_dog(struct dog *d)
 	else
 	{
 		(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-	        printf("Age: %f\n", d->age);
-		(d->owner == NULL) ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
