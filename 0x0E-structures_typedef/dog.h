1d375eff817f1c8a83e1b7fbe22327461f86e275
Minervus00 <fayouedraogo@protonmail.com>
2022-07-06 08:25:54 -0700

    ggqwwz

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,12 +16,18 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 #endif /*#ifndef __STRUCT__VAR__H__*/
 
 #ifndef __AR__H__
 #define __AR__H__
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /*#ifndef __AR__H__*/
 
