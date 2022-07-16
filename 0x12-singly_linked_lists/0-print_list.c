1a54ce3486b7ba5e2cf13ac8c558283bcdf67cb5
Minervus00 <fayouedraogo@protonmail.com>
2022-07-15 17:39:45 -0700

    new

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,40 +5,39 @@
 /**
  * print_list - thaaaat
  * @h: fsqq
- * 
+ *
  * Return: size_t
  */
 size_t print_list(const list_t *h)
 {
-    size_t n = 0;
-    list_t *head;
+	size_t n = 0;
+	list_t *head;
 
-    head = malloc(sizeof(list_t));
-    if (head == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	head = malloc(sizeof(list_t));
+	if (head == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    head->str = h->str;
-    head->len = h->len;
-    head->next = h->next;
-    do
-    {
-        if (n > 0)
-        {
-            head->str = (head->next)->str;
-            head->len = (head->next)->len;
-            head->next = (head->next)->next;
-        }
+	head->str = h->str;
+	head->len = h->len;
+	head->next = h->next;
+	do {
+		if (n > 0)
+		{
+			head->str = (head->next)->str;
+			head->len = (head->next)->len;
+			head->next = (head->next)->next;
+		}
 
-        if (head->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", head->len, head->str);
-        n++;
-    } while (head->next != NULL);
+		if (head->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", head->len, head->str);
+		n++;
+	} while (head->next != NULL);
 
-    free(head);
-    return (n);
+	free(head);
+	return (n);
 }
