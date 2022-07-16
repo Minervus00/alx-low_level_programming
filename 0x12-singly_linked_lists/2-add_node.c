33821e902c4f3dabf4837e7936ed50d4aa8fb7b1
Minervus00 <fayouedraogo@protonmail.com>
2022-07-15 17:49:20 -0700

    hbxc22

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,28 +4,27 @@
 #include <string.h>
 
 /**
- * add_node - thaaaat 
- * 
- * @head: list_t ** 
+ * add_node - thaaaat
+ * @head: list_t **
  * @str: const char *
- * 
+ *
  * Return: list_t *
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
+	list_t *new;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        write(1, "Error\n", 6);
-        exit(1);
-    }
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		write(1, "Error\n", 6);
+		exit(1);
+	}
 
-    new->str = ((str == NULL) ? "(nil)" : strdup(str));
-    new->len = strlen(str);
-    new->next = ((head == NULL) ? NULL : *head);
+	new->str = ((str == NULL) ? "(nil)" : strdup(str));
+	new->len = strlen(str);
+	new->next = ((head == NULL) ? NULL : *head);
 
-    *head = new;
-    return (new);
+	*head = new;
+	return (new);
 }
