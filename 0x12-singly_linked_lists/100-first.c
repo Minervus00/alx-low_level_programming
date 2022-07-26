3b08006ca0a6d28d4a7b6432c56f13d6b5200e72
Minervus00 <fayouedraogo@protonmail.com>
2022-07-26 07:57:09 -0700

    tygqsq

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * premain - pooo
+ *
+ * Return: xx
+ */
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
