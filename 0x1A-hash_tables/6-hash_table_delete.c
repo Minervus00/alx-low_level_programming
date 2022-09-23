97ea349128fd53260dc59d6f6cd06f7316763da2
Minervus00 <fayouedraogo@protonmail.com>
2022-09-23 16:23:05 +0000

    fun

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,22 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	(void) ht;
+	unsigned long int i, size;
+	hash_node_t *node, *tmp;
 
-	return;
+	if (ht == NULL)
+		return;
+	size = ht->size;
+	for (i = 0; i < size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
