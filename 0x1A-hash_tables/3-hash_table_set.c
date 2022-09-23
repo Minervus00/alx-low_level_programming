f26b37d914a48cac2be8c27040d450ac829fc2ff
Minervus00 <fayouedraogo@protonmail.com>
2022-09-23 00:25:34 +0000

    funky

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,9 +15,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	unsigned long int idx;
 
-	/*key cannot be an empty string*/
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
+	/*key cannot be an empty string*/
 	if (!strcmp(key, ""))
 		return (0);
 
