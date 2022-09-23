0cf2be1e13a6da8561071d3b5289e029808752c4
Minervus00 <fayouedraogo@protonmail.com>
2022-09-23 17:12:37 +0000

    funky

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			val = tmp->value;
 			tmp->value = strdup(value);
-			free(new);
+			free(val);
 			return (1);
 		}
 		tmp = tmp->next;
