fa96aa6a2a1f77c01c64f0a3cd2b5c5723ab4982
Minervus00 <fayouedraogo@protonmail.com>
2022-11-11 09:06:23 +0000

    funky

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """Returns the perimeter of an island made with horizontally or
+    vertically conneted cells"""
+    perim = neighbs = 0
+    cells = []
+    tested_cells = []
+    nrows, ncols = len(grid), len(grid[0])
+
+    # Search the first cell of the island
+    for j in range(nrows):
+        for i in range(ncols):
+            if grid[j][i] == 1:
+                # print(f"1st cell: row = {j} | col = {i}")
+                cells.append((j, i))
+                break
+        else:  # Executed if no break in for loop same indented above it
+            continue
+        break
+
+    # Use Breadth First Search starting to the 1st cell of island
+    # to reach all connected cells;
+    # For each cell in cells, count its' neighbors and this cell implication
+    # in the total perimeter is about 4 - number_of_neighbors.
+    while(cells):
+        j, i = cells.pop(0)
+        tested_cells.append((j, i))
+        for y, x in [(j, i-1), (j, i+1), (j-1, i), (j+1, i)]:
+            if 0 <= y < nrows and 0 <= x < ncols and grid[y][x] == 1:
+                if (y, x) not in tested_cells:
+                    cells.append((y, x))
+                neighbs += 1
+        perim += 4 - neighbs
+        neighbs = 0
+
+    return perim
