e8d0d4a808a42897a7b6350f4ff518dd832e999e
Minervus00 <fayouedraogo@protonmail.com>
2023-01-07 10:02:58 +0000

    funky

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,2 +1,51 @@
+#include <stdio.h>
+#include <math.h>
 
+/**
+ * jump_search - searches for a value using jump search
+ * @array: a pointer to the first element of the ascending sorted array
+ * @size: number of elements in the array
+ * @value: the value to search for
+ * Return: the index where 'value' is located
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int a = 0, jmp = (int) sqrt(size), b = jmp;
 
+	if (array == NULL)
+		return (-1);
+
+	if (array[a] == value)
+		return (a);
+	printf("Value checked array[%d] = [%d]\n", a, array[a]);
+
+	/* Encircle the value between 'a' and min(b, n) - 1 */
+	while (array[((b < (int) size) ? b : (int) (size - 1))] < value)
+	{
+		if (b >= (int) size)
+			break;
+		if (array[b] == value)
+			return (b);
+		printf("Value checked array[%d] = [%d]\n", b, array[b]);
+		a = b;
+		b += jmp;
+
+		if (a >= (int) size)
+			return (-1);
+	}
+	printf("Value found between indexes [%d] and [%d]\n", a, b);
+
+	while (array[a] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", a, array[a]);
+		a += 1;
+		if (a == ((b < (int) size) ? b + 1 : (int) size))
+			return (-1);
+	}
+
+	printf("Value checked array[%d] = [%d]\n", a, array[a]);
+	if (array[a] == value)
+		return (a);
+	else
+		return (-1);
+}
---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * interpolation_search - searches for a value using interpolation search
+ * @array: a pointer to the first element of the ascending sorted array
+ * @size: number of elements in the array
+ * @value: the value to search for
+ * Return: the index where 'value' is located
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+
+}
