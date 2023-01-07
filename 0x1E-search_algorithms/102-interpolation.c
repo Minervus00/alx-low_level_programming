cd20cc322ebd3c6eda5248407f953bb159cf3266
Minervus00 <fayouedraogo@protonmail.com>
2023-01-07 11:41:39 +0000

    funky

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,12 +10,13 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1;
-	size_t pos = ((value - array[low]) * (high - low)) / (
-			array[high] - array[low]);
+	size_t pos = 0;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
+	pos = ((value - array[low]) * (high - low)) / (
+			array[high] - array[low]);
 	while ((array[high] != array[low]) && (value > array[low]) && (
 		value <= array[high]))
 	{
