c75747f985a7795c1d3891e50355df34db6871f7
Minervus00 <fayouedraogo@protonmail.com>
2023-01-07 11:29:53 +0000

    funky

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -9,5 +9,35 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
+	size_t low = 0, high = size - 1;
+	size_t pos = ((value - array[low]) * (high - low)) / (
+			array[high] - array[low]);
 
+	if (array == NULL)
+		return (-1);
+
+	while ((array[high] != array[low]) && (value > array[low]) && (
+		value <= array[high]))
+	{
+		printf("Value checked array[%lld] = [%d]\n", pos, array[pos]);
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (array[pos] > value)
+			high = pos - 1;
+		else
+			return (pos);
+		pos = ((value - array[low]) * (high - low)) / (
+			array[high] - array[low]);
+	}
+
+	if (value == array[low])
+	{
+		printf("Value checked array[%lld] = [%d]\n", low, array[low]);
+		return (low);
+	}
+	else
+	{
+		printf("Value checked array[%lld] is out of range\n", pos);
+		return (-1);
+	}
 }
